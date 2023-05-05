a2d2cd1933eb4c34a4acaa312e0c440f62cdb464
debu <tennis.@email.com>
2023-05-05 04:28:23 +0000

    Finish login-function

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,28 @@
+class SessionsController < ApplicationController
+
+  def new
+  end
+
+  def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      log_in user
+      redirect_to user
+    else
+      flash.now[:danger] = '認証に失敗しました。'
+      render :new
+    end
+  end
+  
+  def current_user
+    if session[:user_id]
+    @current_user = @current_user || User.find_by(id: session[:user_id])
+    end
+  end
+  
+  def destroy
+    log_out
+    flash[:success] = 'ログアウトしました。'
+    redirect_to root_url
+  end
+end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,23 +5,23 @@ class UsersController < ApplicationController
   end
 
   def new
-    @user = User.new # ユーザーオブジェクトを生成し、インスタンス変数に代入します。
+    @user = User.new
   end
-  
+
   def create
     @user = User.new(user_params)
     if @user.save
-      # 保存に成功した場合は、ここに記述した処理が実行されます。
+      log_in @user #保存成功後、ログインします。
       flash[:success] = '新規作成に成功しました。'
       redirect_to @user
     else
       render :new
     end
   end
-  
-    private
 
-  def user_params
+  private
+
+    def user_params
       params.require(:user).permit(:name, :email, :password, :password_confirmation)
-  end
+    end
 end
